0793e5be8009bb89cba1c1541a1e44ecd19bfba9
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-01 14:44:04 -0400

    refactor loginUser function

--- controllers/users.js
+++ controllers/users.js
@@ -6,7 +6,18 @@ const createToken = (_id) => {
 }
 
 const loginUser = async (req, res) => {
-    console.log('Log In attempted...')
+    const {email, password} = req.body
+
+    try {
+        const user = await User.login(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
 }
 
 const signupUser = async (req, res) => {
