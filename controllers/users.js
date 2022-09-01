54ffae58ef4acfb8e3ae3434c8ac4e7d1998596a
UbuntuLex <alexandermaniwang99@gmail.com>
2022-08-31 21:13:51 -0400

    create token function using JWTs and signup user saving it to MongoDB and hashing password

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,25 @@
+import { User } from "../models/user.js"
+
+const createToken = (_id) => {
+    return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
+
 const loginUser = async (req, res) => {
     console.log('Log In attempted...')
 }
 
 const signupUser = async (req, res) => {
-    console.log('Sign Up attempted...')
+    const { email, password } = req.body
+    try {
+        const user = await User.signup(email, password)
+
+        // create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({ email, token })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
 }
 
 export {
