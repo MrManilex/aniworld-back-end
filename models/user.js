6b8c88b96d9e234531d05889f0b114262bd8a1b8
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-12 10:53:37 -0400

    refactor user model

--- models/user.js
+++ models/user.js
@@ -13,6 +13,10 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
+    },
+    profile: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Profile"
     }
 })
 
--- routes/profile.js
+++ routes/profile.js
@@ -3,6 +3,7 @@ import * as profilesCtrl from '../controllers/profile.js'
 
 const router = Router()
 
+// Create Profile 
 // Get Profile
 // Get Animes Watching
 // Get Manga Reading
