ab194a824825c01db2241154d28131bd87e27c6a
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-26 16:29:34 -0400

    fix schema and remove comments

--- models/anime.js
+++ models/anime.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema
 
 const animeSchema = new Schema({
     animeTitle: { type: String, required: true },
-    animeId: Number
+    animeId: Number,
+    currentlyWatching: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
+    planningToWatch: [{type: Schema.Types.ObjectId, ref: 'Profile'}]
 })
 
 const Anime = mongoose.model('Anime', animeSchema)
--- models/profile.js
+++ models/profile.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose'
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
-    // email: String,
     username: String,
     avatar: String
-    // watching: String
 },
 {
     timestamps: true
