2ee8a11aca8cfd7f88efc07252643d883a70c541
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-29 11:11:39 -0400

    add coverimage to anime model for front end use

--- models/anime.js
+++ models/anime.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema
 const animeSchema = new Schema({
     animeTitle: { type: String, required: true },
     animeId: Number,
+    coverImage: String,
     currentlyWatching: [{type: Schema.Types.ObjectId, ref: 'Profile'}],
     planningToWatch: [{type: Schema.Types.ObjectId, ref: 'Profile'}]
 })
