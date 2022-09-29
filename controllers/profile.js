9f0330e1c82df0a8a59d0011531f1e4bc2516f15
UbuntuLex <alexandermaniwang99@gmail.com>
2022-09-29 10:48:56 -0400

    retrieve animes with profile id attached to currently watching

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,5 @@
 import { Profile } from '../models/profile.js'
+import { Anime } from '../models/anime.js'
 
 function getProfile(req, res) {
     Profile.findById(req.params.id)
@@ -8,7 +9,12 @@ function getProfile(req, res) {
 }
 
 function getWatching(req, res) {
-    console.log(req.user)
+    // console.log(req.params)
+    // console.log(req.user)
+    Anime.find({ currentlyWatching: req.params.id })
+    .then(animes => {
+        res.json(animes)
+    })
 }
 
 export {
